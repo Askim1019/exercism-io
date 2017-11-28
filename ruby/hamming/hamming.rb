26d1c2e7976cf6728925dd70f356c4c677440208
Alex Kim <askim1019@gmail.com>
2017-11-28 22:29:48 +0000

    get the first four tests to pass from code in hamming.rb

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,8 +1,13 @@
 class Hamming
   def self.compute(strand1, strand2)
-    if strand1.length == strand2.length
-      return 0
+    hamming_distance = 0
+    
+    if strand1 != strand2
+      hamming_distance += 1
     end
     
+    return hamming_distance
   end
+  
+  puts Hamming.compute("A","G") # should return 1
 end
