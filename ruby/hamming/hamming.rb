86a59f99b8752dfaaeb2c2f85fb01cc07601fa49
Alex Kim <askim1019@gmail.com>
2017-11-29 17:50:43 +0000

    extra comments and call result

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,3 +1,6 @@
+# This passes the first 4 tests.
+
+=begin
 class Hamming
   def self.compute(strand1, strand2)
     hamming_distance = 0
@@ -9,5 +12,23 @@ class Hamming
     return hamming_distance
   end
   
-  puts Hamming.compute("A","G") # should return 1
+  puts Hamming.compute("AG","CT") # should return 1
 end
+=end
+
+class Hamming
+  
+  def self.compute(strand1, strand2)
+    hamming_distance = 0
+    
+    strand_one_arr = strand1.to_a
+    strand_two_arr = strand2.to_a
+    strand_one_arr.zip(strand1,)
+  end
+  
+end
+
+
+
+#The call result should be this.
+#hamming('GAGCCTACTAACGGGAT', 'CATCGTAATGACGGCCT') => 7
