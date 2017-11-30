88bd5dddb773ad0264d232e94e69b0042861176f
Alex Kim <askim1019@gmail.com>
2017-11-30 17:21:11 +0000

    set variables and constants

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,8 +1,16 @@
 class GigasecondTest
 
 
-  def self.from(start_date)
-  
+  def self.from(birthday)
+    
+    gigasec = 1_000_000_000
+    
+    #convert birthday into seconds only
+    birthday_conversion = birthday.strftime('%s').to_i
+    
+    gigasecond_date_in_seconds = birthday_conversion + 1_000_000_000
+    
+    gigasecond_conversion_to_utc = 
   end
   
 end
