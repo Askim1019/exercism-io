0f3318dcacb660246791925b4e3694149a972102
Alex Kim <askim1019@gmail.com>
2017-12-03 07:45:14 +0000

    get all tests to pass and add bookkeeping module

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -5,16 +5,35 @@
 class Complement
   
   def self.of_dna(nucleotide)
-    case nucleotide
-    when "G"
-      return "C"
-    when "C"
-      return "G"
-    when "T"
-      return "A"
-    when "A"
-      return "U"
+    list_of_nucleotides = nucleotide.split('') # make string into array
+    
+
+    transcribed_list = [] # assign ruby 
+    list_of_nucleotides.each do |char| # Iterate over array or each letter
+      case char # conditional case statement that transcribes the nucleotide letter
+      when "G"
+        transcribed_list << "C"
+      when "C"
+        transcribed_list << "G"
+      when "T"
+        transcribed_list << "A"
+      when "A"
+        transcribed_list << "U"
+      else
+        transcribed_list = ['']
+        break
+      end
     end
+    
+    transcribed_list.join # join the array elements to make one string
+    
   end
   
 end
+
+module BookKeeping
+  VERSION = 4 # Where the version number matches the one in the test.
+end
+
+Complement.of_dna('ACGTXXXCTTAA')
+
