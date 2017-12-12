9298d8be1ac03153d1873a5477f9103fa0ca1f0b
Alex Kim <askim1019@gmail.com>
2017-12-12 22:37:51 +0000

    Write methods for nth_prime.rb and pass all tests. High numbers take a long time to compile. Change in future

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -2,7 +2,48 @@ class Prime
   
   def self.nth(number)
     
+    if number == 0
+      raise ArgumentError
+    end
     
+    list_of_primes = []
+    total = 0
+    
+    while list_of_primes.length < number
+      if self.factor_total(total) === 2
+        list_of_primes.push(total)
+        total += 1
+      else
+        total += 1
+        next
+      end
+      
+      list_of_primes
+    end
+    
+    list_of_primes.last
   end
   
+  def self.factor_total(x)
+    factors = 0
+    counter = 1
+    
+    while counter <= x
+      if x % counter == 0
+        factors += 1
+      else
+        factors += 0
+      end
+      
+      counter += 1
+      
+    end
+    
+    factors
+  end
+  
+end
+
+module BookKeeping
+  VERSION = 1
 end
